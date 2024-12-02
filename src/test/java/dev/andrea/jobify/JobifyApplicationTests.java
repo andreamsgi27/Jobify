06d077a215d7e83531bcf48bdaca8494e9632d68
andreamsgi27 <andreamsgi27@hotmail.com>
2024-12-02 22:33:45 +0100

    Inicialización del proyecto Spring Boot

---
+++ src/test/java/dev/andrea/jobify/JobifyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andrea.jobify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
