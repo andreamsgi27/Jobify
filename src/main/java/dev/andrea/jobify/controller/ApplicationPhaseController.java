c5152a43923ee0596ccffd5e716fc1a1606a48f7
andreamsgi27 <andreamsgi27@hotmail.com>
2024-12-06 00:32:22 +0100

    "Added custom methods to ApplicationPhaseRepository, and implemented business logic in ApplicationPhaseService."

--- src/main/java/dev/andrea/jobify/controller/ApplicationPhaseController.java
+++ src/main/java/dev/andrea/jobify/controller/ApplicationPhaseController.java
@@ -6,15 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.andrea.jobify.DTO.ApplicationDTO;
 import dev.andrea.jobify.DTO.ApplicationPhaseDTO;
-import dev.andrea.jobify.model.Application;
 import dev.andrea.jobify.service.ApplicationPhaseService;
 
 //Endpoints para consultar las fases de una candidatura específica junto con la fecha de cambio.
