c9f75aec539590ea5cca2c7b0e4214730dc5aa01
andreamsgi27 <andreamsgi27@hotmail.com>
2024-12-05 17:22:26 +0100

    Updated application.properties to work with persistant db without duplicates. Created data.sql to insert predefined values into phase and jobtype tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+-- Valores fijos de JobType
+INSERT INTO job_type (typeId, name) VALUES (1, 'Remote');
+INSERT INTO job_type (typeId, name) VALUES (2, 'Hybrid');
+INSERT INTO job_type (typeId, name) VALUES (3, 'On-Site');
+
+-- Valores fijos de Phase
+INSERT INTO phase (phaseId, name) VALUES (1, 'Exploring');
+INSERT INTO phase (phaseId, name) VALUES (2, 'Submitted');
+INSERT INTO phase (phaseId, name) VALUES (3, 'Reviewing');
+INSERT INTO phase (phaseId, name) VALUES (4, 'Interview');
+INSERT INTO phase (phaseId, name) VALUES (5, 'Tech interview');
+INSERT INTO phase (phaseId, name) VALUES (6, 'Final interview');
+INSERT INTO phase (phaseId, name) VALUES (7, 'Offer');
+INSERT INTO phase (phaseId, name) VALUES (8, 'Hired');
