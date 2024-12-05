9cf8502e2c10e0c32053fe924ed72c06804f0aef
andreamsgi27 <andreamsgi27@hotmail.com>
2024-12-05 22:28:12 +0100

    "ApplicationController and ApplicationPhase completed. ApplicationService structure"

--- src/main/java/dev/andrea/jobify/service/ApplicationPhaseService.java
+++ src/main/java/dev/andrea/jobify/service/ApplicationPhaseService.java
@@ -1,8 +1,28 @@
 package dev.andrea.jobify.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import dev.andrea.jobify.DTO.ApplicationPhaseDTO;
 
 @Service
 public class ApplicationPhaseService {
     
+    public String changePhase(Long applicationId, ApplicationPhaseDTO appPhaseDTO){
+        return "Application phase changed";
+    }
+
+    public ApplicationPhaseDTO getLastPhase(Long applicationId){
+        return new ApplicationPhaseDTO();
+    }
+
+    public List<ApplicationPhaseDTO> getAllPhases(Long applicationId){
+        return new ArrayList<>();
+    }
+
+    public List<ApplicationPhaseDTO> getApplicationsByPhase(Long phaseId){
+        return new ArrayList<>();
+    }
 }
